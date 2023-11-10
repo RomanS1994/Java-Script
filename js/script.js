7511714fbb5680718bc4db1e6e1321f7ea606145
Roman <140334590+RomanS1994@users.noreply.github.com>
2023-11-10 23:29:46 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -49,3 +49,63 @@
 // cena = Number.parseInt(28.39847);
 // cena = cena.toFixed(2);
 // console.log(cena);
+
+
+
+// 3 
+// 4
+// const base = 3
+// const state = 4
+// const result = Math.pow(base, state);
+// // console.log(result);
+// const result = (base ** state);
+// console.log(result);
+
+
+// let base = prompt(`Число`);
+
+// let step = prompt(`ступінь`);
+// const result = base ** step;
+// console.log(result);
+
+// let brend = prompt(`адрес`);
+// brend = brend.toLowerCase();
+// console.log(brend);
+
+// if (true) {
+//     const value = 5;
+//     console.log("Block scope: ", value); 
+//   }
+
+//   if (true) {
+//     const value = 5;
+//     console.log("Global scope: ", value); // 5
+//   }
+  
+
+// let password = "";
+
+// do {
+//   password = prompt("Введіть пароль довший 4-х символів", "");
+// } while (password.length < 5);
+
+// console.log("Ввели пароль: ", password);
+
+// for (let i = 0; i <= 10; i += 2) {
+//     console.log(i);
+//   }
+  
+
+// const target = 3;
+// let sum = 0;
+
+// for (let i = 0; i <= target; i += 1) {
+//   sum += i;
+// }
+
+// console.log(sum);
+
+// const max = 10;
+// for (let i = 0; i < max; i += 1) {
+//   console.log(`${max} % ${i} = `, max % i);
+// }
