5b72f66e41201dee7a4082d8ac0db9a099102331
Roman <140334590+RomanS1994@users.noreply.github.com>
2023-12-01 02:10:59 +0100

    update callback

--- js/callback.js
+++ js/callback.js
@@ -1,14 +1,58 @@
-const doMath = function (a, b, callback) {
-  const result = callback(a, b);
-  console.log(result);
+/**
+  |============================
+  | Синтаксис
+  |============================
+*/
+// const doMath = function (a, b, callback) {
+//   const result = callback(a, b);
+//   console.log(result);
+// };
+
+// const add = function (x, y) {
+//   return x + y;
+// };
+
+// const qwe = function (x, y) {
+//   return x * y;
+// };
+
+// doMath(2, 6, add);
+
+// /**
+//  *
+//  * @param {*} a = x
+//  * @param {*} b = y
+//  * @param {*} callback  = функція
+//  */
+
+// const doMath = function (a, b, callback) {
+//   const result = callback(a, b);
+//   console.log(result);
+// };
+
+// // const add = function (x, y) {
+// //   return x + y;
+// // };
+
+// // const qwe = function (x, y) {
+// //   return x * y;
+// // };
+
+// doMath(2, 6, function (x, y) {
+//   return x * y;
+// });
+
+/**
+  |============================
+  | Приклади
+  |============================
+*/
+/*Реєстрація дійства*/
+
+const buttonRef = document.querySelector(".js-button");
+
+const handleBtnClick = function () {
+  console.log("Click" + Date.now());
 };
 
-const add = function (x, y) {
-  return x + y;
-};
-
-const qwe = function (x, y) {
-  return x * y;
-};
-
-doMath(2, 6, add);
+buttonRef.addEventListener("click", handleBtnClick);
