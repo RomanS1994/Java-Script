54301ca085b2b5759469ded500535f37a9bf62c7
Roman <140334590+RomanS1994@users.noreply.github.com>
2023-12-02 09:07:45 +0100

    Update callback.js

--- js/callback.js
+++ js/callback.js
@@ -49,24 +49,72 @@
 */
 /*Реєстрація дійства*/
 
-const buttonRef = document.querySelector(".js-button");
+// const buttonRef = document.querySelector(".js-button");
 
-const handleBtnClick = function () {
-  console.log("Click" + Date.now());
-};
+// const handleBtnClick = function () {
+//   console.log("Click" + Date.now());
+// };
 
-buttonRef.addEventListener("click", handleBtnClick);
+// buttonRef.addEventListener("click", handleBtnClick);
 
 /**
   |============================
   | виклик геопозиції
   |============================
 */
-const onGetPositionSuccess = function (position) {
-  console.log(position);
-};
-const ongetPositionError = function (error) {
-  console.log(error);
+// const onGetPositionSuccess = function (position) {
+//   console.log(position);
+// };
+// const ongetPositionError = function (error) {
+//   console.log(error);
+// };
+
+// window.navigator.geolocation.getCurrentPosition(onGetPositionSuccess);
+
+/**
+  |============================
+  | SetTimeout 
+  |============================
+*/
+
+// const callback = function () {
+//   console.log("Через 3 секунди всередені колбека в таймауті");
+// };
+
+// console.log("В коді перед таймаутом");
+
+// setTimeout(callback, 3000);
+
+// console.log("В коді після таймаута");
+
+/**
+  |============================
+  |  Робота з бекендом
+  |============================
+*/
+
+// - API URL: https://pokeapi.co/api/v2/pokemon
+
+// const onRequestSuccess = function (reponse) {
+//   console.log(reponse);
+// };
+
+// fetch("https://pokeapi.co/api/v2/pokemon")
+//   .then((res) => res.json())
+//   .then(onRequestSuccess);
+
+/**
+  |============================
+  | Метод для фільтрації масива
+  |============================
+*/
+
+const filter = function (array) {
+  const filteredarray = [];
+
+  for (const el of array) {
+    console.log(el);
+  }
 };
 
-window.navigator.geolocation.getCurrentPosition(onGetPositionSuccess);
+filter([1, 2, 3, 4, 5]);
