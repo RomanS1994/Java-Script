997460575f6c42bbecac1a9ad9b56b51d818126e
Roman <140334590+RomanS1994@users.noreply.github.com>
2023-12-04 18:15:04 +0100

    Update Metod of array.js

--- js/Metod of array.js
+++ js/Metod of array.js
@@ -281,21 +281,167 @@
 // Task-1
 // Напишіть функцію, яка використовує метод map, щоб створити новий масив об'єктів, в якому буде інформація про середній бал кожного студента.
 
+// const students = [
+//   { name: "John", grades: [80, 85, 90] },
+//   { name: "Alice", grades: [90, 95, 92] },
+//   { name: "Bob", grades: [70, 80, 75] },
+//   { name: "Emily", grades: [95, 92, 88] },
+//   { name: "David", grades: [85, 88, 90] },
+// ];
+
+// function getAverage(arr) {
+//   return arr.map(({ name, grades }) => {
+//     const total = grades.reduce((acc, item) => acc + item, 0);
+//     return {
+//       name,
+//       average: Math.round(total / grades.length),
+//     };
+//   });
+// }
+// console.log(getAverage(students));
+
+// const array = [];
+// [...numbers].forEach((number) => {
+//   number % 2 === 0 ? array.push(number + value) : array.push(number);
+// });
+
+// // return array;
+// const planets = ["Earth", "Mars", "Venus", "Jupiter"];
+// // Change code below this line
+
+// const planetsLengths = planets.map((planet) => planet.length);
+// console.log(planetsLengths);
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     genres: ["adventure", "history"],
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     genres: ["fiction"],
+//   },
+//   {
+//     title: "Redder Than Blood",
+//     author: "Tanith Lee",
+//     genres: ["horror", "mysticism"],
+//   },
+// ];
+// // Change code below this line
+
+// const genres = books.flatMap(({ genres }) => genres);
+// console.log(genres);
+// // Change code below this line
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     genres: ["adventure", "history"],
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     genres: ["fiction", "mysticism"],
+//   },
+//   {
+//     title: "Redder Than Blood",
+//     author: "Tanith Lee",
+//     genres: ["horror", "mysticism", "adventure"],
+//   },
+// ];
+// // Change code below this line
+// const allGenres = books.flatMap((book) => book.genres);
+// const uniqueGenres = allGenres.filter(
+//   (course, index, array) => array.indexOf(course) === index
+// );
+// console.log(uniqueGenres);
+
+// const users = [
+//   {
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     gender: "male",
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     gender: "female",
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     gender: "male",
+//   },
+//   {
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     gender: "female",
+//   },
+//   {
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     eyeColor: "blue",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     isActive: true,
+//     balance: 3951,
+//     gender: "male",
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     eyeColor: "brown",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     isActive: false,
+//     balance: 1498,
+//     gender: "male",
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     gender: "female",
+//   },
+// ];
+// // Change code below this line
+// const getUsersWithEyeColor = (users, color) => {
+//   const arrayUsersColors = users.flatMap((user) => user.eyeColor);
+// };
+// // Change code above this line
+// console.log(arrayUsersColors);
 const students = [
-  { name: "John", grades: [80, 85, 90] },
-  { name: "Alice", grades: [90, 95, 92] },
-  { name: "Bob", grades: [70, 80, 75] },
-  { name: "Emily", grades: [95, 92, 88] },
-  { name: "David", grades: [85, 88, 90] },
+  { name: "Mango", score: 83, courses: ["mathematics", "physics"] },
+  { name: "Poly", score: 59, courses: ["science", "mathematics"] },
+  { name: "Ajax", score: 37, courses: ["physics", "biology"] },
+  { name: "Kiwi", score: 94, courses: ["literature", "science"] },
 ];
 
-function getAverage(arr) {
-  return arr.map(({ name, grades }) => {
-    const total = grades.reduce((acc, item) => acc + item, 0);
-    return {
-      name,
-      average: Math.round(total / grades.length),
-    };
-  });
-}
-console.log(getAverage(students));
+const sortedByAscendingScore = [...students].sort((a, b) => a.score - b.score);
+
+console.log(sortedByAscendingScore);
+// Change code below this line
+const getNamesSortedByFriendCount = users => {
+  return [...users].sort((a, b) = a.friends.length - b.friends.length)
+ .map((user) => user.name)
+};
+// Change code above this line
