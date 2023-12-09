6881ff1b8d47b7691bd4847038f83c863630c10d
Roman <140334590+RomanS1994@users.noreply.github.com>
2023-12-09 16:14:45 +0100

    add lekcia

---
+++ js/lekcia.povtor.js
@@ -0,0 +1,33 @@
+// оголошення змінних
+
+// const value = 0;
+
+// let number = 15;
+
+//  примітивні типи даних
+
+// const value = "25";
+
+// console.log( n Number(value));
+
+// ******** Значення які завжди приводятся до false ********
+
+//  0 (Число 0)
+console.log(Boolean(0));
+
+// "" (Порожній ярядок)
+console.log(Boolean(""));
+
+// NaN
+console.log(Boolean(NaN));
+
+// Undefined
+console.log(Boolean(undefined));
+
+// null
+console.log(Boolean(null));
+
+// false
+console.log(Boolean(false));
+
+/******  ******/
