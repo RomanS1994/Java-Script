970cfc8c06cc8c69239b83d7b2ccfffe4fba0b26
Roman <140334590+RomanS1994@users.noreply.github.com>
2023-12-11 23:35:52 +0100

    update freeCode

--- js/freeCodeCamp.js
+++ js/freeCodeCamp.js
@@ -12,3 +12,18 @@
 // const lastLetterOfLastName = lastName[lastName.length - 1]; // Змініть цей рядок
 
 // console.log(lastLetterOfLastName);
+
+function nextInLine(arr, item) {
+  // Змініть код лише під цим рядком
+  item = arr.push(item).shift(arr);
+  return item;
+  // Змініть код лише над цим рядком
+}
+
+// Налаштування
+let testArr = [1, 2, 3, 4, 5];
+
+// Відобразити код
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
